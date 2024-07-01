c1bbcf0cb51d499fe77911bce709ddf6ab915402
Sulaiman1011 <lsulaiman1011@gmail.com>
2024-07-01 12:05:12 +0700

    test

---
+++ latihan1.php
@@ -0,0 +1,12 @@
+<?php
+echo "Hello Word!!";
+//perintah untuk add atau tambah
+//1. git add . atau git add nama_folder/file
+//Perintah utuk commit atau Komentar
+//2. git commit -m "komentarnya apa"
+//perintah untuk upload atau push
+//3. git push -u origin master
+//push adalah perintah, origin adalah nama remote
+//master adalah nama branch
+
+?>
